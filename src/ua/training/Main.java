a98e5a2fec09e0a33ee66c5f0273a24a007f0d50
derfulim <barashenko@ukr.net>
2020-03-14 17:03:52 +0200

    Update Main.java

--- src/ua/training/Main.java
+++ src/ua/training/Main.java
@@ -9,15 +9,15 @@ public class StreamTaskJ8{
 		//Найти минимальный элемент, значение и индекс
 		int min = intStream.min().getAsInt();
 		System.out.println(min);
+int minIndex = IntStream.range(0,arr.length).
+	filter(o->min==arr[o]).
+	findFirst().getAsInt();
+	System.out.println(minIndex);	
 		//Посчитать количество элементов равных нулю
 		//System.out.println(intStream.filter(o -> o ==0).count());
 		//Посчитать количество элементов больше нуля
 		//System.out.println(intStream.filter(o -> o >0).count());
 		//Помножить элементы массива на число
 	//intStream.map(o -> o*2).forEach(System.out::println);
-	int minIndex = IntStream.range(0,arr.length).
-	filter(o->min==arr[o]).
-	findFirst().getAsInt();
-	System.out.println(minIndex);	
 	}
 }
