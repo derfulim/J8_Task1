31f883cd46bd4b440c09c159e993ab9a7bdc71f8
derfulim <barashenko@ukr.net>
2020-03-14 17:01:32 +0200

    Update Main.java

--- src/ua/training/Main.java
+++ src/ua/training/Main.java
@@ -1,14 +1,23 @@
-package ua.training;
-//Задание 1 Stream.
-//        Сделать массив int. Из него получить IntStream. Для него
-//        1. Найти среднее значение элементов массива
-//        2. Найти минимальный элемент, значение и индекс
-//        3. Посчитать количество элементов равных нулю
-//        4. Посчитать количество элементов больше нуля
-//        5. Помножить элементы массива на число
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
+import java.util.stream.*;
+public class StreamTaskJ8{
+	public static void main(String[] args) {
+		//Сделать массив int. Из него получить IntStream.
+		int arr [] = {8,3,5,1};
+		IntStream intStream = IntStream.of(8,3,5,1);
+		//Найти среднее значение элементов массива
+		//System.out.println(intStream.average());
+		//Найти минимальный элемент, значение и индекс
+		int min = intStream.min().getAsInt();
+		System.out.println(min);
+		//Посчитать количество элементов равных нулю
+		//System.out.println(intStream.filter(o -> o ==0).count());
+		//Посчитать количество элементов больше нуля
+		//System.out.println(intStream.filter(o -> o >0).count());
+		//Помножить элементы массива на число
+	//intStream.map(o -> o*2).forEach(System.out::println);
+	int minIndex = IntStream.range(0,arr.length).
+	filter(o->min==arr[o]).
+	findFirst().getAsInt();
+	System.out.println(minIndex);	
+	}
 }
